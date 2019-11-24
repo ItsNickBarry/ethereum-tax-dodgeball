56d4c1129c118a66eb7773bce1a036df16e06f33
Nick Barry <itsnickbarry@protonmail.ch>
2019-11-24 00:10:50 -0600

    add html meta tags

--- saber-browser.js
+++ saber-browser.js
@@ -1,5 +1,12 @@
 export default function (context) {
+  let pkg = require('./package.json');
+
   context.setHead({
     title: 'Ethereum Tax Dodgeball',
+    meta: [
+      { name: 'author', content: pkg.author },
+      { name: 'description', content: pkg.description },
+      { name: 'keywords', content: pkg.keywords.join(', ') },
+    ],
   });
 }
